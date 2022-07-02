f594621ffa57d15a7087726b0db6d431f87d30c0
TRUP03 <truptee20_ug@ee.nits.ac.in>
2022-07-03 02:46:46 +0530

    Admin_logIn

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 //jshint esversion:6
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.listen(8080,()=>{
-    console.log("listenning at port 8080")
-})
-app.get('/',(req,res)=>{
-    // res.send("Finally here...!")
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://Truptee:Truptee123@cluster0.u3q7n.mongodb.net/logIns", {
+    useNewUrlParser: true
+});
+const userSchema = new mongoose.Schema({
+    email: "string",
+    password: "string"
+});
+const User = mongoose.model("User", userSchema);
+const Admin = new User({
+    email: "admin@gmail.com",
+    password: "Admin@123"
+});
+// Admin.save();
+
+
+app.listen(8080, () => {
+    console.log("listenning at port 8080");
+});
+app.get('/', (req, res) => {
     res.render('home.ejs');
-})
-app.get('/login',(req,res)=>{
+});
+app.get('/login', (req, res) => {
     res.render('login.ejs');
-})
-
-app.get('/signin',(req,res)=>{
+});
+app.get('/signin', (req, res) => {
     res.render('signin.ejs');
-})
+});
+app.post('/admin', (req, res) => {
+    const password = req.body.password;
+    User.findOne({
+        _id: "62c0af4e4a3b1e2f99987a6d"
+    }, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else if (result.password === password) {
+            res.render('admin.ejs');
+        }
+    });
+});
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
